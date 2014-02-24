63de15470043359b997a5930ea0dae4e51d6ecc3
Ian Hudson <ian@cel-uk.com>
2014-02-24 11:51:47 +0000

    Initial maven commit

---
+++ src/main/java/libertysystems/stenographer/StenographerFactory.java
@@ -0,0 +1,202 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package libertysystems.stenographer;
+
+import java.io.IOException;
+import java.util.HashMap;
+import libertysystems.configuration.ConfigNotLoadedException;
+import libertysystems.configuration.Configuration;
+import org.apache.log4j.*;
+
+/**
+ *
+ * This class allows Stenographer logging objects to be created.
+ *
+ * Instantiate a Stenographer like this: Stenographer steno =
+ * StenographerFactory.getStenographer("nameToAppearInLog");
+ *
+ * To configure the behaviour of the loggers create Stenographer.properties in
+ * the working directory. The following entries show the default configuration
+ * that will be used if no file is provided (and is in the format required by
+ * the file). logfilename=nologname.log loglevel=INFO logmode=LOCAL logtee=YES
+ *
+ * logfilename - name of file (necessary in local mode) loglevel - one of OFF,
+ * DEBUG, INFO, WARNING or ERROR logmode - LOCAL or REMOTE (LOCAL is to file -
+ * REMOTE is to syslog) logtee - NO or YES (controls whether logging is copied
+ * to the console or not) maxfilesize - value as a string e.g. 10MB or 1GB (the
+ * default is 20MB)
+ *
+ * @author Ian Hudson
+ */
+public class StenographerFactory
+{
+
+    private static HashMap<String, Stenographer> stenographers = new HashMap<String, Stenographer>();
+    private static boolean initialised = false;
+    private static LogLevel initialLogLevel = null;
+
+    private StenographerFactory()
+    {
+    }
+
+    /**
+     * Returns a Stenographer object that provides logging facilities.
+     *
+     * This method will return a new object for each unique logAs string that is
+     * provided. Successive calls to the method with the same string will yield
+     * a reference to the same object.
+     *
+     * @param logAs The string that will appear in the log - usually the
+     * classname e.g. this.getClass().getCanonicalName()
+     *
+     * @return a Stenographer object
+     *
+     *
+     * @see Stenographer
+     */
+    public static Stenographer getStenographer(String logAs)
+    {
+        Stenographer stenoToReturn = null;
+
+        if (!initialised)
+        {
+            if (!initLogger())
+            {
+                System.err.println("Failed to initialise logging factory.");
+            } else
+            {
+                initialised = true;
+            }
+        }
+
+        if (stenographers.containsKey(logAs))
+        {
+
+            stenoToReturn = stenographers.get(logAs);
+        } else
+        {
+
+            stenoToReturn = new Stenographer(logAs, initialLogLevel);
+            stenographers.put(logAs, stenoToReturn);
+        }
+
+        return stenoToReturn;
+    }
+
+    private static boolean initLogger()
+    {
+        boolean success = false;
+        String configComponentName = "Stenographer";
+
+        String defaultLogFile = "nologname.log";
+        String defaultLogLevel = "INFO";
+        String defaultLogMode = "LOCAL";
+        String defaultLogTee = "YES";
+        String defaultMaxFilesize = "20MB";
+
+        String logfilename = null;
+        String initialLoglevelString = null;
+        String logmode = null;
+        String logtee = null;
+        String maxfilesize = null;
+
+        //Set up a basic console logger so the Commons Config stuff doesn't barf
+        Logger rootLogger = Logger.getRootLogger();
+        PatternLayout pattern = new PatternLayout("%d [%t] %p %l - %m%n");
+
+        rootLogger.setLevel(Level.ERROR);
+        rootLogger.addAppender(new ConsoleAppender(pattern));
+
+        try
+        {
+            Configuration sysConfig = Configuration.getInstance();
+            logfilename = sysConfig.getFilenameString(configComponentName, "logfilename", defaultLogFile);
+            initialLoglevelString = sysConfig.getString(configComponentName, "loglevel", defaultLogLevel);
+
+            logmode = sysConfig.getString(configComponentName, "logmode", defaultLogMode);
+            logtee = sysConfig.getString(configComponentName, "logtee", defaultLogTee);
+            maxfilesize = sysConfig.getString(configComponentName, "maxfilesize", defaultMaxFilesize);
+        } catch (ConfigNotLoadedException ex)
+        {
+            System.err.println("Couldn't get Stenographer configuration - config file could not be loaded - using defaults");
+            logfilename = defaultLogFile;
+            initialLoglevelString = defaultLogLevel;
+            logmode = defaultLogMode;
+            logtee = defaultLogTee;
+            maxfilesize = defaultMaxFilesize;
+        }
+
+        initialLogLevel = LogLevel.valueOf(initialLoglevelString);
+
+        // Leave the root logger set at its lowest level - Stenographer will sort out whether to log or not
+        rootLogger.setLevel(Level.WARN);
+
+        if (logmode.equalsIgnoreCase("LOCAL"))
+        {
+            String testFilename = logfilename;
+
+            try
+            {
+                RollingFileAppender rollingAppender = new RollingFileAppender(pattern, testFilename, true);
+                rollingAppender.setMaxFileSize(maxfilesize);
+                rootLogger.addAppender(rollingAppender);
+            } catch (IOException ex)
+            {
+                System.err.println("Stenographer failed to access " + testFilename + " for logging");
+            }
+
+            if (logtee.equalsIgnoreCase("YES"))
+            {
+                rootLogger.addAppender(new ConsoleAppender(pattern));
+            }
+
+            success = true;
+
+        } else
+        {
+            //Not implemented yet - syslog appender
+            System.err.println("Didn't recognise Stenographer mode '" + logmode + "' from config file " + logfilename);
+        }
+        Stenographer steno = new Stenographer("StenographerFactory", initialLogLevel);
+        steno.info(
+                "StenographerFactory initialised with logfile=" + logfilename + " loglevel=" + initialLoglevelString + " logmode=" + logmode);
+
+        return success;
+    }
+
+    /**
+     * Allows the log level for a specific Stenographer to be changed without
+     * using the object directly.
+     *
+     * This method will change the log level for the Stenographer specified by
+     * the loggingEntity string. E.g. if a Stenographer is created with
+     * StenographerFactory.getStenographer("fred"); you can alter the log level
+     * as follows: StenographerFactory.changeLogLevel("fred", LogLevel.OFF);
+     *
+     * @param loggingEntity The string by which the Stenographer is identified
+     * in the log.
+     *
+     * @param newLevel The logging level to be applied to this entity.
+     *
+     * @return true for success and false for failure
+     *
+     * @see Stenographer
+     */
+    public static boolean changeLogLevel(String loggingEntity, LogLevel newLevel)
+    {
+        boolean success = false;
+
+        Stenographer steno = stenographers.get(loggingEntity);
+        if (steno != null)
+        {
+            steno.changeLogLevel(newLevel);
+        } else
+        {
+            success = false;
+        }
+
+        return success;
+    }
+}
---
+++ src/main/java/libertysystems/stenographer/TestLog.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package libertysystems.stenographer;
+
+/**
+ *
+ * @author Ian Hudson
+ * @ Liberty Systems Limited
+ */
+public class TestLog
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        Stenographer steno = StenographerFactory.getStenographer(TestLog.class.getName());
+        steno.info("hello");
+        steno.info("Home:" + System.getProperty("user.home"));
+        String a = System.getProperty("user.home") + "\\/fred/f";
+        steno.info(a);
+        steno.info(a.replaceAll("\\\\", "/"));
+    }
+}
